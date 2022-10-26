06562e321f3ee3ca79e919729af1b5813633dce3
EC2 Default User <ec2-user@ip-172-31-32-163.ap-northeast-1.compute.internal>
2022-10-26 05:33:49 +0000

    create destroy

--- app/controllers/book_controller.rb
+++ app/controllers/book_controller.rb
@@ -1,10 +1,30 @@
 class BookController < ApplicationController
   def new
+    @book=Book.new
+  end
+
+  def create
+    @book=Book.new(book_params)
+    @book.user_id=current_user.id
+    @book.save
+    redirect_to books_path
   end
 
   def index
   end
 
   def show
+  end
+
+  def destroy
+     book = Book.find(params[:id])  # データ（レコード）を1件取得
+    book.destroy  # データ（レコード）を削除
+    redirect_to '/books'  # 投稿一覧画面へリダイレクト
+  end
+ # 投稿データのストロングパラメータ
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
- root to: 'homes#top'
+  get '/' => 'homes#top'
  devise_for :users
 
-  resources :books, only: [:new, :index, :show]
+  resources :books, only: [:new, :create, :index, :show, :destroy]
 
-  get "homes/about" =>"homes#about",as:'about'
  end
