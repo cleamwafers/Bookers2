3179a8738e506301085b298c69afa0750279588a
EC2 Default User <ec2-user@ip-172-31-32-163.ap-northeast-1.compute.internal>
2022-10-26 14:11:50 +0000

    投稿の編集、削除ボタンは投稿したユーザにのみ表示

--- app/controllers/book_controller.rb
+++ app/controllers/book_controller.rb
@@ -17,8 +17,8 @@ class BookController < ApplicationController
   end
 
   def destroy
-     book = Book.find(params[:id])  # データ（レコード）を1件取得
-    book.destroy  # データ（レコード）を削除
+     @book = Book.find(params[:id])  # データ（レコード）を1件取得
+    @book.destroy  # データ（レコード）を削除
     redirect_to '/books'  # 投稿一覧画面へリダイレクト
   end
  # 投稿データのストロングパラメータ
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get '/' => 'homes#top'
  devise_for :users
 
-  resources :books, only: [:new, :create, :index, :show, :destroy]
+resources :books, only: [:new, :create, :index, :show, :destroy]
 
  end
