0abb2d7ff73d936ba6c3abf5d9c1587daacd23cf
EC2 Default User <ec2-user@ip-172-31-32-163.ap-northeast-1.compute.internal>
2022-10-27 14:17:54 +0000

    topページ整える　途中

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   get '/' => 'homes#top'
+  get '/homes/about' => 'homes#about'
+  get '/users/sign_up' => ''
+  get '/users/sign_in' => ''
+  get '/users' => ''
+  get '/books' => 'books#index'
+  
  devise_for :users
 
 resources :books, only: [:new, :create, :index, :show, :destroy]
